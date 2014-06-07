8325fe210f38644ad10578b950543bbae3f3e00a
amitav13 <amitav.khandelwal@gmail.com>
2014-06-07 19:47:43 -0400

    deleted wave demo, starting UI

--- SnipNote2/src/com/example/snipnote2/MainActivity.java
+++ SnipNote2/src/com/example/snipnote2/MainActivity.java
@@ -17,10 +17,8 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		System.out.println("Creating Wave demo");
 		
-		WaveDemo demo = new WaveDemo();
-		demo.demo();
+		
 	
 	}
 
