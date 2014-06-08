2f09f1a3b21496ad505036cdd32a27bc6bc99d33
fatality-ish <fatality.ish@gmail.com>
2014-06-08 00:43:15 -0400

    Merging the two projects

--- SnipNote2/src/com/example/snipnote2/MainActivity.java
+++ SnipNote2/src/com/example/snipnote2/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.snipnote2;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,23 +18,52 @@ import android.widget.Button;
 public class MainActivity extends ActionBarActivity {
 
 	private Button contactsButton;
+	
+	private static final int REQUEST_CODE = 0;
+    private DevicePolicyManager mDPM;
+    private ComponentName mAdminName;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
 	    getActionBar().hide();
-	    setContentView(R.layout.activity_main);
+	    setContentView(R.layout.activity_main);    
+	    
 	    
 	    contactsButton = (Button)findViewById(R.id.contactsButton);
 	    
 	    contactsButton.setOnClickListener(new OnClickListener(){
 	    	public void onClick(View V){
-	    		Uri uri = Uri.parse("tel:3393689972");
+	    		Uri uri = Uri.parse("tel:8139973626");
 	        	Intent i = new Intent(Intent.ACTION_CALL,uri);
 	            startActivity(i); 
 	    	}
 	    });
+	    
+	    
+	    try {
+            // Initiate DevicePolicyManager.
+            mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+            mAdminName = new ComponentName(this, DeviceAdminDemo.class);
+
+            if (!mDPM.isAdminActive(mAdminName)) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Click on Activate button to secure your application.");
+                startActivityForResult(intent, REQUEST_CODE);
+                Log.i("Logs","working");
+            } else {
+                 //mDPM.lockNow();
+                 //Intent intent = new Intent(MainActivity.this,
+                	//	 TService.class);
+                // startService(intent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("Logs", "Exception in Device Admin Found");
+        } 
 	
 	}
 
@@ -54,4 +87,15 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (REQUEST_CODE == requestCode) {
+        		Log.i("Logs", "Tservice has started");
+                Intent intent = new Intent(MainActivity.this, TService.class);
+                startService(intent);
+        }
+    }
+	
 }
