0c0c4fab27f65f328bc0d36137e77be41143a4a1
amitav13 <amitav.khandelwal@gmail.com>
2014-06-07 20:29:50 -0400

    added home screen interface

--- SnipNote2/src/com/example/snipnote2/MainActivity.java
+++ SnipNote2/src/com/example/snipnote2/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.snipnote2;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.Window;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,7 +12,8 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+	    getActionBar().hide();
 		
 	
 	}
